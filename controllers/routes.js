fb55ae348f1f17a6bb87a9c7134efb2e53ecdc11
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-08-07 23:27:39 -0700

    fix async issue

--- controllers/routes.js
+++ controllers/routes.js
@@ -52,8 +52,8 @@ module.exports = function (app) {
                     }
                 })
             })
-        })
 
-        res.status(200);
+            res.redirect("/");
+        })
     });
 }
